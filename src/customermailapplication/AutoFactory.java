745b7369851fe8e0377bdeeabc45d22a0020e0e6
60050273 <37170875+Sorakung@users.noreply.github.com>
2020-08-25 23:49:17 +0700

    Add files via upload

---
+++ src/customermailapplication/AutoFactory.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package customermailapplication;
+
+/**
+ *
+ * @author black
+ */
+public class AutoFactory {
+    public static Customer createMailFactory(String name){
+        
+        switch (name) {
+            case "Regular":
+                return new RegularCustomer();
+            case "Mountain":
+                return new MountainCustomer();
+            case "Delinquent":
+                return new DelinquentCustomer();
+        }
+        
+            return null;
+        }
+    }
+    
